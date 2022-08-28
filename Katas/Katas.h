e910064e758707c931da89d3b3b1cb262f049a88
generic-keith <keith.staines.ext@bbc.co.uk>
2022-08-28 20:46:40 +0100

    Initial Commit

---
+++ Katas/Katas.h
@@ -0,0 +1,18 @@
+//
+//  Katas.h
+//  Katas
+//
+//  Created by Keith Staines on 28/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Katas.
+FOUNDATION_EXPORT double KatasVersionNumber;
+
+//! Project version string for Katas.
+FOUNDATION_EXPORT const unsigned char KatasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Katas/PublicHeader.h>
+
+
